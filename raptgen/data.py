2a6046daea0d9a1cff57779ecef1836c853eb0da
Natsuki IWANO <n.iwano0630@gmail.com>
2023-03-19 12:38:27 +0000

    add pre-commit

--- raptgen/data.py
+++ raptgen/data.py
@@ -1,34 +1,39 @@
+"""This module is used to create data, manage data for sequential input.
+"""
 from enum import IntEnum
-from typing import List
+from typing import List, Union, Tuple
+import numpy as np
+
 
 class State(IntEnum):
-    """Enum for Profile HMM. M stands for Match, I stands for insertion and D stands for deletion."""
+    """Enum for Profile HMM.
+    M stands for Match, I stands for insertion and D stands for deletion."""
 
     M = 0
-    Match = 0
+    MATCH = 0
     I = 1
-    Insertion = 1
+    INSERTION = 1
     D = 2
-    Deletion = 2
+    DELETION = 2
 
 
 class Transition(IntEnum):
     """Enum for State-to-State transition."""
 
     M2M = 0
-    Match_to_Match = 0
+    MATCH_TO_MATCH = 0
     M2I = 1
-    Match_to_Insertion = 1
+    MATCH_TO_INSERTION = 1
     M2D = 2
-    Match_to_Deletion = 2
+    MATCH_TO_DELETION = 2
     I2M = 3
-    Insertion_to_Match = 3
+    INSERTION_TO_MATCH = 3
     I2I = 4
-    Insertion_to_Insertion = 4
+    INSERTION_TO_INSERTION = 4
     D2M = 5
-    Deletion_to_Match = 5
+    DELETION_TO_MATCH = 5
     D2D = 6
-    Deletion_to_Deletion = 6
+    DELETION_TO_DELETION = 6
 
 
 class Nucleotide(IntEnum):
@@ -39,9 +44,18 @@ class Nucleotide(IntEnum):
     U = 1
     G = 2
     C = 3
+    N = 4
     PAD = 4
     SOS = 5
     EOS = 6
+
+
+class SNV(IntEnum):
+    """Enum for Single Nucleotide Variant"""
+
+    MUTATION = 0
+    INSERTION = 1
+    DELETION = 2
 
 
 def one_hot_index(seq: str) -> List[int]:
@@ -54,3 +68,30 @@ def one_hot_index(seq: str) -> List[int]:
         List[int]: list of sequence converted into int
     """
     return [int(Nucleotide[char]) for char in seq]
+
+
+def one_hot_encode(seq: str, padding: Union[int, Tuple[int, int]] = 0) -> np.array:
+    """create one_hot encodes sequence from sequence string.
+
+    Args:
+        seq (str): sequence created using A, T(U), G, C.
+        padding (Union[int, Tuple[int, int]], optional): length of the padding sequence.
+        If tuple of int is provided, the first number used to pad the start,
+        and the other used to pad the end. Defaults to 0.
+
+    Returns:
+        np.array: array of sequence. The first dim refers to the sequence length,
+        and the second dim refers to the nucleotide index.
+    """
+    # inside array, index 0 to 3 is one_hot and index 4 is all 0.25
+    arr = np.vstack((np.eye(4), np.ones(4)[None, :] * 0.25))
+
+    # split padding into left and right
+    if isinstance(padding, int):
+        paddings = (padding, padding)
+    else:
+        paddings = padding
+    left_pad, right_pad = paddings
+
+    # return one hot array
+    return arr[one_hot_index("N" * left_pad + seq + "N" * right_pad)]
--- tests/unit/test_data.py
+++ tests/unit/test_data.py
@@ -1,20 +1,90 @@
 from context import raptgen
-from raptgen import data
+from raptgen.data import one_hot_encode, one_hot_index
 import torch
+import numpy as np
 
 
 def test_one_hot_index():
     # Test case 1
     seq = "ATGC"
     expected_output = [0, 1, 2, 3]
-    assert data.one_hot_index(seq) == expected_output
+    assert one_hot_index(seq) == expected_output
 
     # Test case 2
     seq = "CTAG"
     expected_output = [3, 1, 0, 2]
-    assert data.one_hot_index(seq) == expected_output
+    assert one_hot_index(seq) == expected_output
 
     # Test case 3
     seq = "AGCT"
     expected_output = [0, 2, 3, 1]
-    assert data.one_hot_index(seq) == expected_output
+    assert one_hot_index(seq) == expected_output
+
+
+def test_one_hot_encode():
+    # Test case 1
+    seq = "ATGC"
+    expected_output = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+    assert np.array_equal(one_hot_encode(seq), expected_output)
+
+    # Test case 2
+    seq = "CTAG"
+    expected_output = np.array([[0, 0, 0, 1], [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0]])
+    assert np.array_equal(one_hot_encode(seq), expected_output)
+
+    # Test case 3
+    seq = "AGCT"
+    expected_output = np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [0, 1, 0, 0]])
+    assert np.array_equal(one_hot_encode(seq), expected_output)
+
+    # Test case 4
+    seq = "ATGC"
+    padding = 2
+    expected_output = np.array(
+        [
+            [0.25, 0.25, 0.25, 0.25],
+            [0.25, 0.25, 0.25, 0.25],
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+            [0.25, 0.25, 0.25, 0.25],
+            [0.25, 0.25, 0.25, 0.25],
+        ]
+    )
+    assert np.array_equal(one_hot_encode(seq, padding), expected_output)
+
+    # Test case 5
+    seq = "AGCT"
+    padding = (2, 3)
+    expected_output = np.array(
+        [
+            [0.25, 0.25, 0.25, 0.25],
+            [0.25, 0.25, 0.25, 0.25],
+            [1, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+            [0, 1, 0, 0],
+            [0.25, 0.25, 0.25, 0.25],
+            [0.25, 0.25, 0.25, 0.25],
+            [0.25, 0.25, 0.25, 0.25],
+        ]
+    )
+    assert np.array_equal(one_hot_encode(seq, padding), expected_output)
+
+    # Test case 6
+    seq = "AGCT"
+    padding = (3, 2)
+    expected_output = np.array(
+        [
+            [0.25, 0.25, 0.25, 0.25],
+            [0.25, 0.25, 0.25, 0.25],
+            [0.25, 0.25, 0.25, 0.25],
+            [1, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 1],
+            [0.25, 0.25, 0.25, 0.25],
+            [0.25, 0.25, 0.25, 0.25],
+        ]
+    )
